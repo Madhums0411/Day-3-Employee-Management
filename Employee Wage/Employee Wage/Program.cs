209811d8ef1599cafe6f0465a606b85714e28097
madhums0411 <madhums41175@gmail.com>
2022-07-16 16:36:44 +0530

    Part time Employee Wage

--- Employee Wage/Employee Wage/Program.cs
+++ Employee Wage/Employee Wage/Program.cs
@@ -1,22 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Employee Wage Problem");
 {
-    int IS_FULL_TIME = 1;
+    int IS_PART_TIME = 1;
+    int IS_FULL_TIME = 2;
     int EMP_RATE_PER_HOUR = 20;
 
     int emphours = 0;
     int empwage = 0;
 
     Random random = new Random();
-    int empcheck = random.Next(0, 2);
-    if (empcheck == IS_FULL_TIME)
+    int empcheck = random.Next(0, 3);
+    if (empcheck == IS_PART_TIME)
+    {
+        emphours = 4;
+    }
+    else if (empcheck == IS_FULL_TIME)
     {
         emphours = 8;
     }
     else
     {
         emphours = 0;
-
     }
     empwage = emphours * EMP_RATE_PER_HOUR;
     Console.WriteLine("Employee wage is : " + empwage);
