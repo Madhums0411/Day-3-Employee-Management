79b349d36a96d06e794393129408f9806e428514
madhums0411 <madhums41175@gmail.com>
2022-07-16 16:15:09 +0530

    Employee Present or Absent

--- Employee Wage/Employee Wage/Program.cs
+++ Employee Wage/Employee Wage/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Employee Wage Problem");
+{
+    int IS_FULL_TIME = 1;
+
+    Random random = new Random();
+    int empcheck = random.Next(0, 2);
+    if (empcheck == IS_FULL_TIME)
+    {
+        Console.WriteLine("Employee is Present");
+    }
+    else
+    {
+        Console.WriteLine("Employee is Absent");
+
+    }
+}
