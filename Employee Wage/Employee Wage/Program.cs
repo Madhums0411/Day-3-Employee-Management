eb4251057354c8fc30a3e5832cf234fe3273a4c9
madhums0411 <madhums41175@gmail.com>
2022-07-16 17:03:13 +0530

    Employee monthly wage

--- Employee Wage/Employee Wage/Program.cs
+++ Employee Wage/Employee Wage/Program.cs
@@ -3,30 +3,38 @@
     public const int IS_PART_TIME = 1;
     public const int IS_FULL_TIME = 2;
     public const int EMP_RATE_PER_HOUR = 20;
+    public const int NUM_OF_WORKING_DAYS = 20;
 
     static void Main(string[] args)
     {
 
         int emphours = 0;
         int empwage = 0;
+        int totalempwage = 0;
 
-        Random random = new Random();
-        int empcheck = random.Next(0, 3);
-        switch (empcheck)
+        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
         {
-            case IS_PART_TIME:
-                emphours = 4;
-                break;
-            case IS_FULL_TIME:
-                emphours = 8;
-                break;
-            default:
-                emphours = 0;
-                break;
 
+            Random random = new Random();
+            int empcheck = random.Next(0, 3);
+            switch (empcheck)
+            {
+                case IS_PART_TIME:
+                    emphours = 4;
+                    break;
+                case IS_FULL_TIME:
+                    emphours = 8;
+                    break;
+                default:
+                    emphours = 0;
+                    break;
+
+            }
+
+            empwage = emphours * EMP_RATE_PER_HOUR;
+            totalempwage += empwage;
+            Console.WriteLine("Employee wage is : " + empwage);
         }
-
-        empwage = emphours * EMP_RATE_PER_HOUR;
-        Console.WriteLine("Employee wage is : " + empwage);
+        Console.WriteLine("Total Employee wage is : " + totalempwage);
     }
 }
