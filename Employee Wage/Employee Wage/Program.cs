f7cbca56cc247d2c8f677f96a22c818bf81b0648
madhums0411 <madhums41175@gmail.com>
2022-07-16 16:44:17 +0530

    Using Switch case for employee wage

--- Employee Wage/Employee Wage/Program.cs
+++ Employee Wage/Employee Wage/Program.cs
@@ -1,27 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Employee Wage Problem");
+﻿class program
 {
-    int IS_PART_TIME = 1;
-    int IS_FULL_TIME = 2;
-    int EMP_RATE_PER_HOUR = 20;
+    public const int IS_PART_TIME = 1;
+    public const int IS_FULL_TIME = 2;
+    public const int EMP_RATE_PER_HOUR = 20;
 
-    int emphours = 0;
-    int empwage = 0;
+    static void Main(string[] args)
+    {
 
-    Random random = new Random();
-    int empcheck = random.Next(0, 3);
-    if (empcheck == IS_PART_TIME)
-    {
-        emphours = 4;
+        int emphours = 0;
+        int empwage = 0;
+
+        Random random = new Random();
+        int empcheck = random.Next(0, 3);
+        switch (empcheck)
+        {
+            case IS_PART_TIME:
+                emphours = 4;
+                break;
+            case IS_FULL_TIME:
+                emphours = 8;
+                break;
+            default:
+                emphours = 0;
+                break;
+
+        }
+
+        empwage = emphours * EMP_RATE_PER_HOUR;
+        Console.WriteLine("Employee wage is : " + empwage);
     }
-    else if (empcheck == IS_FULL_TIME)
-    {
-        emphours = 8;
-    }
-    else
-    {
-        emphours = 0;
-    }
-    empwage = emphours * EMP_RATE_PER_HOUR;
-    Console.WriteLine("Employee wage is : " + empwage);
 }
