aac65b5f80b39f42bd733fcba156b6e4a69f4b7d
madhums0411 <madhums41175@gmail.com>
2022-08-05 20:18:10 +0530

    Multiple company wage calculation

--- Employee Wage/Employee Wage/Program.cs
+++ Employee Wage/Employee Wage/Program.cs
@@ -1,4 +1,19 @@
 ﻿using EmployeeWageProblem;
 
-EmployeeWage employeeWage = new EmployeeWage();
-employeeWage.CalculateEmpWage();
+Console.WriteLine("1 For TATA Company Employee Wage");
+Console.WriteLine("2 For MAHINDRA Company Employee Wage");
+
+int problem = Convert.ToInt32(Console.ReadLine());
+switch (problem)
+{
+    case 1:
+        EmployeeWageComputation employeewagecomputation = new EmployeeWageComputation();
+        employeewagecomputation.AddCompany("TATA", 20, 8, 4, 100, 20);
+        employeewagecomputation.CalculateEmpWage("TATA");
+        break;
+    case 2:
+        EmployeeWageComputation wagecomputation = new EmployeeWageComputation();
+        wagecomputation.AddCompany("MAHINDRA", 30, 8, 4, 100, 20);
+        wagecomputation.CalculateEmpWage("MAHINDRA");
+        break;
+}
