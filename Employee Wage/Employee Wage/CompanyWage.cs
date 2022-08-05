f8ec80501f950630822477443a9f13ec10b7075e
madhums0411 <madhums41175@gmail.com>
2022-08-05 20:40:26 +0530

    Employee wage using interface class

--- Employee Wage/Employee Wage/CompanyWage.cs
+++ Employee Wage/Employee Wage/CompanyWage.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Employee_Wage
+namespace EmployeeWageProblem
 {
     public class Company
     {
--- Employee Wage/Employee Wage/EmployeeWage.cs
+++ Employee Wage/Employee Wage/EmployeeWage.cs
@@ -1,4 +1,4 @@
-﻿using Employee_Wage;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
