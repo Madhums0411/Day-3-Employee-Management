74d8fb4949f3a581bda09e972ff52b8c208e0f0d
madhums0411 <madhums41175@gmail.com>
2022-08-05 20:06:06 +0530

    Refactor the code of Employee wage

--- Employee Wage/Employee Wage/Program.cs
+++ Employee Wage/Employee Wage/Program.cs
@@ -1,42 +1,4 @@
-﻿class program
-{
-    public const int IS_PART_TIME = 1;
-    public const int IS_FULL_TIME = 2;
-    public const int EMP_RATE_PER_HOUR = 20;
-    public const int NUM_OF_WORKING_DAYS = 20;
-    public const int MAX_WORKING_HOURS = 100;
+﻿using EmployeeWageProblem;
 
-    static void Main(string[] args)
-    {
-
-        int emphours = 0;
-        int totalemphours = 0;
-        int totalworkingdays = 0;
-
-        while (totalemphours <= MAX_WORKING_HOURS && totalworkingdays < NUM_OF_WORKING_DAYS)
-        {
-            totalworkingdays++;
-            Random random = new Random();
-            int empcheck = random.Next(0, 3);
-            switch (empcheck)
-            {
-                case IS_PART_TIME:
-                    emphours = 4;
-                    break;
-                case IS_FULL_TIME:
-                    emphours = 8;
-                    break;
-                default:
-                    emphours = 0;
-                    break;
-
-            }
-
-            totalemphours += emphours;
-            Console.WriteLine("Day: " + totalworkingdays + "Employee hours: " + emphours);
-
-        }
-        int totalempwage = totalemphours * EMP_RATE_PER_HOUR;
-        Console.WriteLine("Total Employee wage is : " + totalempwage);
-    }
-}
+EmployeeWage employeeWage = new EmployeeWage();
+employeeWage.CalculateEmpWage();
